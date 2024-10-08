e2cb9bf1c8b86cc281ab0cec1ac51968b207d8d5
Yago Ribeiro <yagoribeiro21@gmail.com>
2024-10-08 11:22:14 -0300

    create teste

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text, Button, TextInput } from "react-native";
+
+export default function HomeScreen() {
+  // Valide o email do usuario e a senha para que sejam os mesmos abaixo;
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Text>E-mail</Text>
+      <TextInput></TextInput>
+      <Text>Senha</Text>
+      <TextInput></TextInput>
+      <Button title="Enviar" />
+    </View>
+  );
+}
